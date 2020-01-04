26da57356c6370eefb6857d686dc81a39a46e95f
Renzo Davoli <renzo@cs.unibo.it>
2020-01-04 18:22:34 +0100

    update scripts for the tutorial

--- archive/install_scripts/install_packets_for_v2.sh
+++ archive/install_scripts/install_packets_for_v2.sh
@@ -4,8 +4,9 @@ apt-get install git python3 build-essential cmake make autogen \
 	autoconf libtool libcap-dev libattr1-dev libfuse-dev \
 	libexecs-dev libmhash-dev libpam0g-dev libssl-dev \
 	libfuse-dev e2fsprogs comerr-dev e2fslibs-dev xterm \
-	x11-utils libpcap-dev libslirp-dev vim vde2 vdetelweb
+	udhcpc x11-utils libpcap-dev libslirp-dev vim \
+	vde2 vdetelweb
 
-ln -s /bin/false /bin/nologin
+ln -sf /bin/false /bin/nologin
 echo 'kernel.unprivileged_userns_clone=1' > /etc/sysctl.d/00-local-userns.conf
 service procps restart
