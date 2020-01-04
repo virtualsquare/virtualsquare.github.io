38a9cda224ee7cb60443b0646a748b3275bc62a3
Renzo Davoli <renzo@cs.unibo.it>
2020-01-04 17:55:22 +0100

    update install scripts for the tutorial

--- archive/install_scripts/install_packets_for_v2.sh
+++ archive/install_scripts/install_packets_for_v2.sh
@@ -2,9 +2,9 @@
 
 apt-get install git python3 build-essential cmake make autogen \
 	autoconf libtool libcap-dev libattr1-dev libfuse-dev \
-	libexecs-dev libmhash-dev libpam-dev libssl-dev \
+	libexecs-dev libmhash-dev libpam0g-dev libssl-dev \
 	libfuse-dev e2fsprogs comerr-dev e2fslibs-dev xterm \
-	x11-utils libslirp vim vde2 vdetelweb
+	x11-utils libslirp-dev vim vde2 vdetelweb
 
 echo 'kernel.unprivileged_userns_clone=1' > /etc/sysctl.d/00-local-userns.conf
 service procps restart
