e3b16f2c3f20ae3e88c10af8c550cfdb4dce7a14
Renzo Davoli <renzo@cs.unibo.it>
2024-11-21 13:26:59 +0100

    install_packets_for_v2 updated

--- archive/install_scripts/install_packets_for_v2.sh
+++ archive/install_scripts/install_packets_for_v2.sh
@@ -3,9 +3,9 @@
 apt-get --yes install git python3 build-essential cmake make autogen \
 	autoconf libtool libcap-dev libattr1-dev libfuse-dev libwolfssl-dev \
 	libexecs-dev libmhash-dev libpam0g-dev libssl-dev netcat-openbsd netcat-traditional \
-	libfuse-dev e2fsprogs comerr-dev e2fslibs-dev xterm \
+	libfuse3-dev e2fsprogs comerr-dev e2fslibs-dev xterm \
 	udhcpc x11-utils libpcap-dev libslirp-dev vim gzip \
-	vde2 vdetelweb archivemount libarchive-dev sshfs
+	vde2 vdetelweb archivemount libarchive-dev sshfs fuse2fs
 
 ln -sf /bin/false /bin/nologin
 echo 'kernel.unprivileged_userns_clone=1' > /etc/sysctl.d/00-local-userns.conf
