ff0364df5d2f64c32348423ec4c11f8d5c8df36c
Renzo Davoli <renzo@cs.unibo.it>
2025-09-09 17:54:54 +0200

    install scripts: add libglib2.0-dev package dep

--- archive/install_scripts/install_packets_for_v2.sh
+++ archive/install_scripts/install_packets_for_v2.sh
@@ -3,7 +3,7 @@
 apt-get --yes install git python3 build-essential cmake make autogen \
 	autoconf libtool libcap-dev libattr1-dev libfuse-dev libwolfssl-dev \
 	libexecs-dev libmhash-dev libpam0g-dev libssl-dev netcat-openbsd netcat-traditional \
-	libfuse3-dev e2fsprogs comerr-dev e2fslibs-dev xterm \
+	libfuse3-dev e2fsprogs comerr-dev e2fslibs-dev xterm libglib2.0-dev \
 	udhcpc x11-utils libpcap-dev libslirp-dev vim gzip \
 	vde2 vdetelweb archivemount libarchive-dev sshfs fuse2fs uuid-dev libblkid-dev
 
